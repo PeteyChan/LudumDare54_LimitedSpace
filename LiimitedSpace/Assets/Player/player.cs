56a1962aa005ae1144a6f6b4471515227fe36f66
petey <akivsfyuu@gmail.com>
2023-10-01 19:21:00 +1000

    added some burners

--- LiimitedSpace/Assets/Player/player.cs
+++ LiimitedSpace/Assets/Player/player.cs
@@ -3,132 +3,144 @@ using System;
 
 public partial class player : RigidBody3D
 {
-	[Export] Inputs up = Inputs.key_w, down = Inputs.key_s, left = Inputs.key_a, right = Inputs.key_d, dash = Inputs.key_space;
-	[Export] float limitsX = 13f, limitsY = 7f;
-	[Export] float speed = 10f;
-	MeshInstance3D player_mesh;
+    [Export] Inputs up = Inputs.key_w, down = Inputs.key_s, left = Inputs.key_a, right = Inputs.key_d, dash = Inputs.key_space;
+    [Export] float limitsX = 13f, limitsY = 7f;
+    [Export] float speed = 10f;
+    MeshInstance3D player_mesh;
 
-	Utils.Statemachine<PlayerStates> state = new();
+    Utils.Statemachine<PlayerStates> state = new();
 
-	public override void _Ready()
-	{
-		if (!this.TryFind(out player_mesh))
-			Debug.LogError("failed to find player mesh");
+    Node3D burner;
 
-		BodyEntered += (body) =>
-		{
-			switch (body)
-			{
-				case Grinder:
-					if (state.current is not PlayerStates.Death)
-						state.next = PlayerStates.Death;
-					break;
+    public override void _Ready()
+    {
+        foreach (var sprite in this.FindAll<AnimatedSprite3D>())
+        {
+            sprite.Play();
+            burner = sprite.GetParent() as Node3D;
+        }
 
-				case Debris debris:
-					if (debris.Mass > 15)
-						SFX.HeavyImpact.Play(Position);
-					else SFX.LightImpact.Play(Position);
-					break;
+        if (!this.TryFind(out player_mesh))
+            Debug.LogError("failed to find player mesh");
 
-				case Wall:
-					SFX.LightImpact.Play(Position);
-					break;
-			}
-		};
-	}
+        BodyEntered += (body) =>
+        {
+            switch (body)
+            {
+                case Grinder:
+                    if (state.current is not PlayerStates.Death)
+                        state.next = PlayerStates.Death;
+                    break;
 
-	PinJoint3D joint;
+                case Debris debris:
+                    if (debris.Mass > 15)
+                        SFX.HeavyImpact.Play(Position);
+                    else SFX.LightImpact.Play(Position);
+                    break;
 
-	float dash_timer;
+                case Wall:
+                    SFX.LightImpact.Play(Position);
+                    break;
+            }
+        };
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _PhysicsProcess(double _delta)
-	{
-		var delta = (float)_delta;
-		Debug.DrawArrow3D(GlobalPosition, player_mesh.GlobalTransform.GetForward(), Colors.Yellow);
+    PinJoint3D joint;
 
-		switch (state.Update(delta))
-		{
-			case PlayerStates.Move:
+    float dash_timer;
 
-				var target_velocity = new Vector3();
-				if (up.Pressed())
-					target_velocity = Vector3.Up;
-				if (down.Pressed())
-					target_velocity += Vector3.Down;
-				if (left.Pressed())
-					target_velocity += Vector3.Left;
-				if (right.Pressed())
-					target_velocity += Vector3.Right;
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _PhysicsProcess(double _delta)
+    {
+        var delta = (float)_delta;
+        //Debug.DrawArrow3D(GlobalPosition, player_mesh.GlobalTransform.GetForward(), Colors.Yellow);
 
-				target_velocity *= speed;
-				target_velocity = LinearVelocity.Lerp(target_velocity, delta * 2f);
+        switch (state.Update(delta))
+        {
+            case PlayerStates.Move:
 
-				if (target_velocity.X * delta + GlobalPosition.X > limitsX)
-					target_velocity.X = target_velocity.X.MaxValue(-0.1f);
+                var target_velocity = new Vector3();
+                if (up.Pressed())
+                    target_velocity = Vector3.Up;
+                if (down.Pressed())
+                    target_velocity += Vector3.Down;
+                if (left.Pressed())
+                    target_velocity += Vector3.Left;
+                if (right.Pressed())
+                    target_velocity += Vector3.Right;
 
-				if (target_velocity.X * delta + GlobalPosition.X < -limitsX)
-					target_velocity.X = target_velocity.X.MinValue(0.1f);
+                target_velocity *= speed;
+                target_velocity = LinearVelocity.Lerp(target_velocity, delta * 2f);
 
-				if (target_velocity.Y * delta + GlobalPosition.Y > limitsY)
-					target_velocity.Y = target_velocity.Y.MaxValue(-0.1f);
+                if (target_velocity.X * delta + GlobalPosition.X > limitsX)
+                    target_velocity.X = target_velocity.X.MaxValue(-0.1f);
 
-				if (target_velocity.Y * delta + GlobalPosition.Y < -limitsY)
-					target_velocity.Y = target_velocity.Y.MinValue(0.1f);
+                if (target_velocity.X * delta + GlobalPosition.X < -limitsX)
+                    target_velocity.X = target_velocity.X.MinValue(0.1f);
 
-				LinearVelocity = target_velocity;
-				LookAtMouse();
+                if (target_velocity.Y * delta + GlobalPosition.Y > limitsY)
+                    target_velocity.Y = target_velocity.Y.MaxValue(-0.1f);
 
-				if ((dash_timer -= delta) < 0 && dash.OnPressed())
-					state.next = PlayerStates.Dash;
-				break;
+                if (target_velocity.Y * delta + GlobalPosition.Y < -limitsY)
+                    target_velocity.Y = target_velocity.Y.MinValue(0.1f);
 
-			case PlayerStates.Death:
-				LinearVelocity = LinearVelocity.Lerp(default, delta * 2f);
+                LinearVelocity = target_velocity;
+                LookAtMouse();
 
-				Debug.Label("Dead", state.current_time).SetColor(Colors.Red);
+                if ((dash_timer -= delta) < 0 && dash.OnPressed())
+                    state.next = PlayerStates.Dash;
 
-				if (state.current_time > 5f)
-				{
-					GameScenes.GameOver.Load();
-					Debug.Log("Go to Game Over");
-				}
-				break;
 
-			case PlayerStates.Dash:
-				dash_timer = .5f;
-				LinearVelocity = player_mesh.GlobalTransform.GetForward() * 20f;
-				if (state.current_time > .1f)
-					state.next = PlayerStates.Move;
-				break;
+                burner.Scale = burner.Scale.Lerp(Vector3.One, delta * 5f);
+                break;
 
-			default:
-				state.next = PlayerStates.Move;
-				break;
-		}
+            case PlayerStates.Death:
+                LinearVelocity = LinearVelocity.Lerp(default, delta * 2f);
 
-		void LookAtMouse()
-		{
-			var target_direction = GetMousePosition() - Position;
-			var current_direction = this.Transform.GetForward();
-			player_mesh.GlobalTransform = player_mesh.GlobalTransform.LookingAt(GlobalPosition - target_direction, Vector3.Forward);
-		}
-	}
+                Debug.Label("Dead", state.current_time).SetColor(Colors.Red);
 
-	static Vector3 GetMousePosition()
-	{
-		Plane plane = new Plane(Vector3.Forward);
-		var viewport = Scene.Current.GetViewport();
-		var camera = viewport.GetCamera3D();
-		var from = camera.ProjectRayOrigin(viewport.GetMousePosition());
-		var dir = camera.ProjectRayNormal(viewport.GetMousePosition());
-		return plane.IntersectsRay(from, dir).GetValueOrDefault();
-	}
+                if (state.current_time > 5f)
+                {
+                    GameScenes.GameOver.Load();
+                    Debug.Log("Go to Game Over");
+                }
+                break;
+
+            case PlayerStates.Dash:
+                burner.Scale = new Vector3(1, 1, 4);
+                dash_timer = 2f;
+                LinearVelocity = player_mesh.GlobalTransform.GetForward() * 20f;
+                if (state.current_time > .1f)
+                    state.next = PlayerStates.Move;
+                break;
+
+            default:
+                state.next = PlayerStates.Move;
+                break;
+        }
+
+        void LookAtMouse()
+        {
+            var target_direction = GetMousePosition() - Position;
+            var current_direction = this.Transform.GetForward();
+            player_mesh.GlobalTransform = player_mesh.GlobalTransform.LookingAt(GlobalPosition - target_direction, Vector3.Forward);
+        }
+    }
+
+    static Vector3 GetMousePosition()
+    {
+        Plane plane = new Plane(Vector3.Forward);
+        var viewport = Scene.Current.GetViewport();
+        var camera = viewport.GetCamera3D();
+        var from = camera.ProjectRayOrigin(viewport.GetMousePosition());
+        var dir = camera.ProjectRayNormal(viewport.GetMousePosition());
+        return plane.IntersectsRay(from, dir).GetValueOrDefault();
+    }
 }
 
 public enum PlayerStates
 {
-	Move,
-	Death,
-	Dash,
+    Move,
+    Death,
+    Dash,
 }
